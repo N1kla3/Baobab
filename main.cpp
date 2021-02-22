eb18f2be094ecb941edb35a0180e88ee4365356b
Nicola <kolya.vladimirsky@gmail.com>
2021-02-22 14:21:35 +0300

    builded :)

---
+++ main.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+
+#include "antlr4-runtime.h"
+#include "generated/TLexer.h"
+#include "generated/TParser.h"
+
+using namespace antlrcpptest;
+using namespace antlr4;
+
+int main(int , const char **) {
+  ANTLRInputStream input(u8"🍴 = 🍐 + \"😎\";(((x * π))) * µ + ∰; a + (x * (y ? 0 : 1) + z);");
+  TLexer lexer(&input);
+  CommonTokenStream tokens(&lexer);
+
+  tokens.fill();
+  for (auto token : tokens.getTokens()) {
+    std::cout << token->toString() << std::endl;
+  }
+
+  TParser parser(&tokens);
+  tree::ParseTree* tree = parser.main();
+
+  std::cout << tree->toStringTree(&parser) << std::endl << std::endl;
+
+  return 0;
+}
